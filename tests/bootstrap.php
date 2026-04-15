11a8e9ebf89d85dd417a0c684ee979843bbb9e94
Giles Partington <gilespartington@giless-macbook-pro.local>
2026-04-15 00:27:00 -0400

    Removing wordpress install

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,22 +2,27 @@
 
 use WorDBless\Load;
 
-if (! file_exists( dirname(__DIR__) . '/wordpress/wp-content')) {
-	mkdir(dirname(__DIR__) . '/wordpress/wp-content');
+// Keep WP core location configurable to match composer.json "wordpress-install-dir".
+$wordpressInstallDir = getenv('WORDPRESS_INSTALL_DIR') ?: '../../../';
+$wordpressPath = dirname(__DIR__) . '/' . $wordpressInstallDir;
+$wpContentPath = $wordpressPath . '/wp-content';
+
+if (! file_exists($wpContentPath)) {
+	mkdir($wpContentPath, 0777, true);
 }
 
-if (! file_exists(dirname(__DIR__) . '/wordpress/wp-content/themes')) {
-	mkdir(dirname(__DIR__) . '/wordpress/wp-content/themes');
+if (! file_exists($wpContentPath . '/themes')) {
+	mkdir($wpContentPath . '/themes', 0777, true);
 }
 
 copy(
     dirname( __DIR__ ) . '/vendor/automattic/wordbless/src/dbless-wpdb.php',
-    dirname( __DIR__ ) . '/wordpress/wp-content/db.php'
+    $wpContentPath . '/db.php'
 );
 
 $theme_base_name = basename( dirname( __DIR__ ) );
 $src = realpath( dirname( dirname( __DIR__ ) ) . '/' . $theme_base_name );
-$dest = dirname( __DIR__ ) . '/wordpress/wp-content/themes/' . $theme_base_name;
+$dest = $wpContentPath . '/themes/' . $theme_base_name;
 
 if ( is_dir($src) && ! file_exists($dest) ) {
 	symlink($src, $dest);
